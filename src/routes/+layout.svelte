a28c10b16f5f3c534388b5059092afbdf014141f
naughtmarrow <naughtmarrow@gmail.com>
2025-10-06 00:05:55 -0600

    feature <global> added a css reset

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+import favicon from "$lib/assets/favicon.svg";
+import '../app.css'
 
-	let { children } = $props();
+let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<h1>This is a global layout</h1>
 
+<h1>This is a global layout</h1>
 {@render children?.()}
