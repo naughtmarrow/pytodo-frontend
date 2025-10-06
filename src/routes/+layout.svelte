a3c5e6e9f6e886a4005924853309b6b0e43acbb3
naughtmarrow <naughtmarrow@gmail.com>
2025-10-06 01:14:01 -0600

    feature: <global> added global variables for main colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,3 +12,17 @@ let { children } = $props();
 
 <h1>This is a global layout</h1>
 {@render children?.()}
+
+<style>
+  :global(body) {
+    background-color: var(--global-background);
+    background-image: var(--global-background-gradient);
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100vh;
+  }
+
+  h1 {
+    color: var(--global-foreground);
+  }
+</style>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+let username: string = $state("")
+let password: string = $state("")
+</script>
+
+<div class="login">
+  <input type="text" bind:value={username}/>
+  <input type="text" bind:value={password}/>
+</div>
+
+<style>
+</style>
